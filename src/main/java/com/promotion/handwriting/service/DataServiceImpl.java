7a9eebcfb63caa545772c21463a60c3dd3cec0e5
yudonggeun <ydong98@gmail.com>
2022-12-09 13:47:31 +0900

    change getFile method into getInputStream method because it causes an error when reading files in the jar package.

--- src/main/java/com/promotion/handwriting/service/DataServiceImpl.java
+++ src/main/java/com/promotion/handwriting/service/DataServiceImpl.java
@@ -7,10 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.LinkedList;
+import java.io.*;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -18,58 +18,41 @@ public class DataServiceImpl implements DataService {
 
     @Override
     public List<String> getImageSrcByContentId(String id, int start, int count) throws IOException {
-        File imageDir = new ClassPathResource("/static/image/content/" + id).getFile();
-
-        List<String> result = new LinkedList<>();
-
-        File[] images = imageDir.listFiles((f, name) -> name.endsWith("jpg") || name.endsWith("png"));
-
-        if (images == null) throw new RuntimeException("image path is not correct");
-
-        for (int i = start; i < start + count && i < images.length; i++) {
-            File image = images[i];
-            result.add("/image/content/" + id + "/" + image.getName());
-        }
-
-        return result;
+        BufferedReader imageReader = new BufferedReader(new InputStreamReader(
+                new ClassPathResource("/static/image/content/" + id).getInputStream()));
+        List<String> allImages = imageReader.lines()
+                .filter(name -> name.endsWith("jpg") || name.endsWith("png"))
+                .map(name -> "/image/content/" + id + "/" + name)
+                .collect(Collectors.toList());
+        int endPoint = Math.min(allImages.size(), start + count);
+        return allImages.subList(start, endPoint);
     }
 
     @Override
     public List<MainPromotionContentDto> readMainPromotionContentTextFile() {
-        List<MainPromotionContentDto> result = new LinkedList<>();
         try {
-            ClassPathResource resource = new ClassPathResource("/text/content/");
-            String imagePath = "/static/image/content/";
+            BufferedReader br = new BufferedReader(new InputStreamReader(new ClassPathResource("/text/content/").getInputStream()));
+            return br.lines().map(fileName -> {
+                        try {
+                            InputStream file = new ClassPathResource("/text/content/" + fileName).getInputStream();
+                            MainPromotionContentDto dto = new ObjectMapper().readValue(new InputStreamReader(file), MainPromotionContentDto.class);
 
-            File dir = resource.getFile();
-            File[] files = dir.listFiles((dir1, name) -> name.endsWith("json"));
-
-            if (files != null) {
-                for (File file : files) {
-                    MainPromotionContentDto dto = new ObjectMapper().readValue(file, MainPromotionContentDto.class);
-
-                    File imageDir = new ClassPathResource(imagePath + dto.getId()).getFile();
-                    File[] images = imageDir.listFiles((f, name) -> name.endsWith("jpg") || name.endsWith("png"));
-
-                    if (images == null) continue;
-                    int repeat = Math.min(images.length, 8);
-                    log.debug("image list size : " + dto.getImages().size());
-                    log.debug(file.getName() + " 조회 : " + dto);
-
-                    for (int i = 0; i < repeat; i++) {
-                        File image = images[i];
-                        dto.addImage("/image/content/" + dto.getId() + "/" + image.getName());
-                    }
-                    log.debug("image directory : " + imageDir.getName());
-                    log.debug("repeat : " + repeat);
-                    log.debug("image list size : " + dto.getImages().size());
-                    log.debug("dto hash : " + dto.hashCode());
-                    log.debug(file.getName() + " 조회 : " + dto);
-                    result.add(dto);
-                }
-            }
-
-            return result;
+                            getImageSrcByContentId(dto.getId(), 0, 8).forEach(image ->
+                                    dto.addImage("/image/content/" + dto.getId() + "/" + image)
+                            );
+                            log.debug("image list size : " + dto.getImages().size());
+                            log.debug(dto.getId() + ".json 조회 : " + dto);
+                            log.debug("image directory : " + "/static/image/content/" + dto.getId());
+                            log.debug("image list size : " + dto.getImages().size());
+                            log.debug("dto hash : " + dto.hashCode());
+                            return dto;
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            return null;
+                        }
+                    })
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -80,11 +63,10 @@ public class DataServiceImpl implements DataService {
     public MainPromotionIntroDto readMainPromotionIntroTextFile() {
         try {
             ClassPathResource resource = new ClassPathResource("/text/intro/intro.json");
-
-            File file = resource.getFile();
+            InputStream file = resource.getInputStream();
             MainPromotionIntroDto result = new ObjectMapper().readValue(file, MainPromotionIntroDto.class);
             result.setImage("/image/intro/" + result.getImage());
-            log.debug(file.getName() + " 조회 : " + result);
+            log.debug("intro.json" + " 조회 : " + result);
 
             return result;
         } catch (IOException e) {
