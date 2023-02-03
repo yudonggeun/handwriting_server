dab72fedc1bdbeca5f2ecaf37aae642fafa9a634
yudonggeun <ydong98@gmail.com>
2023-02-03 18:02:34 +0900

    ci/cd environment configuration files edit

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM adoptopenjdk/openjdk11
 CMD ["./mvnw", "clean", "package"]
 
 RUN mkdir /app
-WORKDIR /app
 #ADD ./build/libs .
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "handwriting-0.0.1-SNAPSHOT.jar"]
+
+ENTRYPOINT ["java", "-jar", "/app/spring-webapp.jar", ">", "/app/log/application.log", "2>", "/app/log/error.log"]
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/spring/handwriting_server"
+PROJECT_ROOT="/home/ubuntu/spring"
+PROJECT_SOURCE_DIRECTORY_PATH="/home/ubuntu/spring/handwriting_server";
+
 JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 
-APP_LOG="$PROJECT_ROOT/application.log"
-ERROR_LOG="$PROJECT_ROOT/error.log"
+APP_LOG="$PROJECT_ROOT/application/log/application.log"
+ERROR_LOG="$PROJECT_ROOT/application/log/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+cp $PROJECT_SOURCE_DIRECTORY_PATH/build/libs/handwriting-0.0.1-SNAPSHOT.jar $JAR_FILE
 
-# jar 파일 실행
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
-
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+# 도커 컨테이너 새로운 빌드.jar 로 재실행
+echo "$TIME_NOW > $JAR_FILE 도커 재실행" >> $DEPLOY_LOG
+sudo docker restart $(docker ps -aqf name=handwriting_spring)
