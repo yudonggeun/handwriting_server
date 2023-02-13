c5997f765989a0a148553c2e2badf69e1bb01198
yudonggeun <ydong98@gmail.com>
2023-02-13 10:39:25 +0900

    modify application.yml to hide secret information.

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,9 +12,9 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 # build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+echo "$TIME_NOW : $JAR_FILE 파일 복사" : $DEPLOY_LOG
 cp $PROJECT_SOURCE_DIRECTORY_PATH/build/libs/handwriting-0.0.1-SNAPSHOT.jar $JAR_FILE
 
 # 도커 컨테이너 새로운 빌드.jar 로 재실행
-echo "$TIME_NOW > $JAR_FILE 도커 재실행" >> $DEPLOY_LOG
+echo "$TIME_NOW : $JAR_FILE 도커 재실행" : $DEPLOY_LOG
 sudo docker restart $(sudo docker ps -aqf name=handwriting_spring)
