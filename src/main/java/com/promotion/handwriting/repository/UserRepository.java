79ca3f32ac32b9ace00a22eb82397f2d4ff5cd36
yudonggeun <ydong98@gmail.com>
2023-02-18 10:05:07 +0900

    update kakao login

--- src/main/java/com/promotion/handwriting/repository/UserRepository.java
+++ src/main/java/com/promotion/handwriting/repository/UserRepository.java
@@ -2,10 +2,15 @@ package com.promotion.handwriting.repository;
 
 import com.promotion.handwriting.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findByUserId(String userId);
+
+    @Override
+    @Query(value = "select count(*) from (select * from user limit 0, 1) a", nativeQuery = true)
+    long count();
 }
