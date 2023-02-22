f52e8e6ecd9f9cda129e5354d28dd7f991f3e43c
yudonggeun <ydong98@gmail.com>
2023-02-22 09:33:48 +0900

    start.sh fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,7 +12,6 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 # build 파일 복사
-<<<<<<< HEAD
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >>$DEPLOY_LOG
 cp $PROJECT_SOURCE_DIRECTORY_PATH/build/libs/handwriting-0.0.1-SNAPSHOT.jar $JAR_FILE
 
@@ -36,11 +35,3 @@ else
   echo "$TIME_NOW > 도커 재실행" >>$DEPLOY_LOG
   sudo docker restart $(sudo docker ps -aqf name=handwriting_spring)
 fi
-=======
-echo "$TIME_NOW : $JAR_FILE 파일 복사" : $DEPLOY_LOG
-cp $PROJECT_SOURCE_DIRECTORY_PATH/build/libs/handwriting-0.0.1-SNAPSHOT.jar $JAR_FILE
-
-# 도커 컨테이너 새로운 빌드.jar 로 재실행
-echo "$TIME_NOW : $JAR_FILE 도커 재실행" : $DEPLOY_LOG
-sudo docker restart $(sudo docker ps -aqf name=handwriting_spring)
->>>>>>> dev
